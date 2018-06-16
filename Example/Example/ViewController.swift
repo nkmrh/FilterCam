ec8313febec9f76cbe6bf666c95766e2aa088b5b
nkmrh <hajimeet@me.com>
2018-06-16 14:38:43 +0900

    Add project files

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by hajime-nakamura on 2018/06/16.
+//  Copyright © 2018 hajime-nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FilterCam/FilterCam.h
@@ -0,0 +1,19 @@
+//
+//  FilterCam.h
+//  FilterCam
+//
+//  Created by hajime-nakamura on 2018/06/16.
+//  Copyright © 2018 hajime-nakamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FilterCam.
+FOUNDATION_EXPORT double FilterCamVersionNumber;
+
+//! Project version string for FilterCam.
+FOUNDATION_EXPORT const unsigned char FilterCamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FilterCam/PublicHeader.h>
+
+
